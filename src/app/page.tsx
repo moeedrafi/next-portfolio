6a6f44d07d7658861e2820ebbc08d5e2c17000af
Moeed Rafi <moeedrafi95@gmail.com>
2024-07-26 20:10:20 +0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
-import Project from "@/components/Project";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-10 lg:p-24 gap-20">
       <Hero />
       <Experience />
-      <Project />
       <Skills />
     </main>
   );
